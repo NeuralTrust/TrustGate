f42d889f74551bcd5aa1fbbd639b11ccb73e2cb8
edu <edu.camacho@neuraltrust.ai>
2025-10-08 13:28:09 +0200

    rollback toxicity plugin

--- pkg/plugins/neuraltrust_toxicity/data.go
+++ pkg/plugins/neuraltrust_toxicity/data.go
@@ -1,6 +1,17 @@
 package neuraltrust_toxicity
 
-type ToxicityData struct {
+type ToxicityDataV2 struct {
 	ToxicityThreshold float64            `json:"toxicity_threshold"`
 	Categories        map[string]float64 `json:"scores"`
+}
+
+// Legacy
+
+type ToxicityData struct {
+	ToxicityThreshold float64 `json:"toxicity_threshold"`
+	Scores            *Scores `json:"scores"`
+}
+
+type Scores struct {
+	Toxicity float64 `json:"toxicity"`
 }
