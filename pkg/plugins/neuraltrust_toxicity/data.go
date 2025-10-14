d6de1156909a68c8812d93667b859a5398c21f7e
edu <edu.camacho@neuraltrust.ai>
2025-10-14 09:20:56 +0200

    new moderation service

--- pkg/plugins/neuraltrust_toxicity/data.go
+++ pkg/plugins/neuraltrust_toxicity/data.go
@@ -1,17 +1,6 @@
 package neuraltrust_toxicity
 
-type ToxicityDataV2 struct {
+type ToxicityData struct {
 	ToxicityThreshold float64            `json:"toxicity_threshold"`
 	Categories        map[string]float64 `json:"scores"`
-}
-
-// Legacy
-
-type ToxicityData struct {
-	ToxicityThreshold float64 `json:"toxicity_threshold"`
-	Scores            *Scores `json:"scores"`
-}
-
-type Scores struct {
-	Toxicity float64 `json:"toxicity"`
 }
--- pkg/plugins/neuraltrust_toxicity/responses.go
+++ pkg/plugins/neuraltrust_toxicity/responses.go
@@ -1,22 +1,6 @@
 package neuraltrust_toxicity
 
-type ToxicityResponseV2 struct {
+type ToxicityResponse struct {
 	Categories map[string]float64 `json:"categories"`
 	Flagged    bool               `json:"flagged"`
-}
-
-// Legacy
-
-type ToxicityResponse struct {
-	Prompt  ToxicityPrompt `json:"categories"`
-	Scores  ToxicityScores `json:"category_scores"`
-	Flagged bool           `json:"flagged"`
-}
-
-type ToxicityPrompt struct {
-	ToxicPrompt bool `json:"toxic_prompt"`
-}
-
-type ToxicityScores struct {
-	ToxicPrompt float64 `json:"toxic_prompt"`
 }
