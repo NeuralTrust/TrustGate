ce37846d65a7b530d666518810a9918190cd0ae3
edu <edu.camacho@neuraltrust.ai>
2025-12-01 17:46:54 +0100

    improve injector protection plugin

--- scripts/generate_jwt_token.sh
+++ scripts/generate_jwt_token.sh
@@ -2,7 +2,7 @@
 
 header=$(echo -n '{"alg":"HS256","typ":"JWT"}' | openssl base64 -A | tr '+/' '-_' | tr -d '=')
 payload=$(echo -n '{}' | openssl base64 -A | tr '+/' '-_' | tr -d '=')
-secret=482fd5986989ac30ad671fed9da8b1bf
+secret=${SERVER_SECRET_KEY}
 
 signature=$(echo -n "$header.$payload" | openssl dgst -binary -sha256 -hmac "$secret" | openssl base64 -A | tr '+/' '-_' | tr -d '=')
 
