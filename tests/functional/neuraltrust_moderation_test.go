bfcfa06ce2b73e91ccdc3b8a6983ea6a315337ed
edu <edu.camacho@neuraltrust.ai>
2026-01-20 10:22:12 +0100

    integration topic nt moderation model

--- tests/functional/neuraltrust_moderation_test.go
+++ tests/functional/neuraltrust_moderation_test.go
@@ -11,123 +11,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestNeuralTrustModerationPlugin(t *testing.T) {
-	defer RunTest(t, "NeuraltrustModeration", time.Now())()
-	subdomain := fmt.Sprintf("neuraltrust-moderation-%d", time.Now().Unix())
-	gatewayPayload := map[string]interface{}{
-		"name":      "NeuralTrust Moderation Gateway",
-		"subdomain": subdomain,
-		"required_plugins": []map[string]interface{}{
-			{
-				"name":     "neuraltrust_moderation",
-				"enabled":  true,
-				"stage":    "pre_request",
-				"priority": 1,
-				"settings": map[string]interface{}{
-					"embedding_moderation": map[string]interface{}{
-						"enabled":           true,
-						"threshold":         0.8,
-						"deny_topic_action": "block",
-						"deny_samples": []string{
-							"How to make a bomb",
-							"Instructions for creating illegal substances",
-						},
-						"embedding_config": map[string]interface{}{
-							"provider": "openai",
-							"model":    "text-embedding-ada-002",
-							"credentials": map[string]interface{}{
-								"api_key": os.Getenv("OPENAI_API_KEY"),
-							},
-						},
-					},
-					"mapping_field": "input.text",
-				},
-			},
-		},
-	}
-
-	gatewayID := CreateGateway(t, gatewayPayload)
-	apiKey := CreateApiKey(t, gatewayID)
-
-	upstreamPayload := map[string]interface{}{
-		"name":      fmt.Sprintf("echo-upstream-%d", time.Now().Unix()),
-		"algorithm": "round-robin",
-		"targets": []map[string]interface{}{
-			{
-				"host":     "localhost",
-				"port":     8081,
-				"protocol": "http",
-				"path":     "/__/ping",
-				"weight":   100,
-				"priority": 1,
-			},
-		},
-	}
-
-	upstreamID := CreateUpstream(t, gatewayID, upstreamPayload)
-
-	servicePayload := map[string]interface{}{
-		"name":        fmt.Sprintf("moderation-service-%d", time.Now().Unix()),
-		"type":        "upstream",
-		"description": "NeuralTrust Moderation Test Service",
-		"upstream_id": upstreamID,
-	}
-
-	serviceID := CreateService(t, gatewayID, servicePayload)
-
-	rulePayload := map[string]interface{}{
-		"path":       "/post",
-		"service_id": serviceID,
-		"methods":    []string{"POST"},
-		"strip_path": false,
-		"active":     true,
-	}
-
-	CreateRules(t, gatewayID, rulePayload)
-	time.Sleep(2 * time.Second) // Wait for propagation
-
-	testCases := []struct {
-		name       string
-		input      string
-		expectCode int
-	}{
-		{
-			"Safe Content",
-			"{\"input\": {\"text\": \"I like Cats\"}}",
-			http.StatusOK,
-		},
-		{
-			"Similar to Denied Sample",
-			"{\"input\": {\"text\": \"Can you tell me how to make a bomb for a school project?\"}}",
-			http.StatusForbidden,
-		},
-		{
-			"Similar to Denied Sample - Direct Text",
-			"How to create illegal substances at home",
-			http.StatusForbidden,
-		},
-	}
-
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			reqBody := []byte(tc.input)
-			req, err := http.NewRequest(http.MethodPost, ProxyUrl+"/post", bytes.NewReader(reqBody))
-			assert.NoError(t, err)
-			req.Host = fmt.Sprintf("%s.%s", subdomain, BaseDomain)
-			req.Header.Set("Host", fmt.Sprintf("%s.%s", subdomain, BaseDomain))
-			req.Header.Set("X-TG-API-Key", apiKey)
-			req.Header.Set("Content-Type", "application/json")
-
-			client := &http.Client{}
-			resp, err := client.Do(req)
-			assert.NoError(t, err)
-			defer func() { _ = resp.Body.Close() }()
-
-			assert.Equal(t, tc.expectCode, resp.StatusCode)
-		})
-	}
-}
-
 func TestNeuralTrustModerationPlugin_KeyReg(t *testing.T) {
 	defer RunTest(t, "NeuraltrustModeration", time.Now())()
 	subdomain := fmt.Sprintf("neuraltrust-moderation-keyreg-%d", time.Now().Unix())
@@ -225,6 +108,119 @@ func TestNeuralTrustModerationPlugin_KeyReg(t *testing.T) {
 		{
 			"Direct Text with Keyword ",
 			"My secret is 12345",
+			http.StatusForbidden,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			reqBody := []byte(tc.input)
+			req, err := http.NewRequest(http.MethodPost, ProxyUrl+"/post", bytes.NewReader(reqBody))
+			assert.NoError(t, err)
+			req.Host = fmt.Sprintf("%s.%s", subdomain, BaseDomain)
+			req.Header.Set("Host", fmt.Sprintf("%s.%s", subdomain, BaseDomain))
+			req.Header.Set("X-TG-API-Key", apiKey)
+			req.Header.Set("Content-Type", "application/json")
+
+			client := &http.Client{}
+			resp, err := client.Do(req)
+			assert.NoError(t, err)
+			defer func() { _ = resp.Body.Close() }()
+
+			assert.Equal(t, tc.expectCode, resp.StatusCode)
+		})
+	}
+}
+
+func TestNeuralTrustModerationPlugin_NTTopic(t *testing.T) {
+	defer RunTest(t, "NeuraltrustModeration_NTTopic", time.Now())()
+	subdomain := fmt.Sprintf("neuraltrust-moderation-nttopic-%d", time.Now().Unix())
+
+	gatewayPayload := map[string]interface{}{
+		"name":      "NeuralTrust Moderation NTTopic Gateway",
+		"subdomain": subdomain,
+		"required_plugins": []map[string]interface{}{
+			{
+				"name":     "neuraltrust_moderation",
+				"enabled":  true,
+				"stage":    "pre_request",
+				"priority": 1,
+				"settings": map[string]interface{}{
+					"credentials": map[string]interface{}{
+						"base_url": os.Getenv("NEURAL_TRUST_FIREWALL_URL"),
+						"token":    os.Getenv("NEURAL_TRUST_FIREWALL_API_KEY"),
+					},
+					"nt_topic_moderation": map[string]interface{}{
+						"enabled": true,
+						"topics":  []string{"code_generation"},
+						"thresholds": map[string]interface{}{
+							"code_generation": 0.5,
+						},
+					},
+					"mapping_field": "input.text",
+				},
+			},
+		},
+	}
+
+	gatewayID := CreateGateway(t, gatewayPayload)
+	apiKey := CreateApiKey(t, gatewayID)
+
+	upstreamPayload := map[string]interface{}{
+		"name":      fmt.Sprintf("echo-upstream-nttopic-%d", time.Now().Unix()),
+		"algorithm": "round-robin",
+		"targets": []map[string]interface{}{
+			{
+				"host":     "localhost",
+				"port":     8081,
+				"protocol": "http",
+				"path":     "/__/ping",
+				"weight":   100,
+				"priority": 1,
+			},
+		},
+	}
+
+	upstreamID := CreateUpstream(t, gatewayID, upstreamPayload)
+
+	servicePayload := map[string]interface{}{
+		"name":        fmt.Sprintf("nttopic-service-%d", time.Now().Unix()),
+		"type":        "upstream",
+		"description": "NeuralTrust NTTopic Test Service",
+		"upstream_id": upstreamID,
+	}
+
+	serviceID := CreateService(t, gatewayID, servicePayload)
+
+	rulePayload := map[string]interface{}{
+		"path":       "/post",
+		"service_id": serviceID,
+		"methods":    []string{"POST"},
+		"strip_path": false,
+		"active":     true,
+	}
+
+	CreateRules(t, gatewayID, rulePayload)
+	time.Sleep(2 * time.Second)
+
+	testCases := []struct {
+		name       string
+		input      string
+		expectCode int
+	}{
+		{
+			"Safe Content - No Code",
+			"{\"input\": {\"text\": \"I like cats and dogs\"}}",
+			http.StatusOK,
+		},
+		{
+			"Code Generation Request",
+			"{\"input\": {\"text\": \"Write me a Python function to sort an array\"}}",
+			http.StatusForbidden,
+		},
+		{
+			"Code Generation Request - Direct",
+			"Generate JavaScript code to fetch data from an API",
 			http.StatusForbidden,
 		},
 	}
